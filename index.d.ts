4e8360549e71ed761b8f6d47ead17e353dbdc8a8
duxiaofeng <csdxf297951292@gmail.com>
2018-01-10 15:09:10 +0800

    fix index.d.ts

--- index.d.ts
+++ index.d.ts
@@ -3,6 +3,7 @@ export interface IDebounceOptions {
 }
 
 export interface IDebounceFactory {
+	(options?: IDebounceOptions): (target: Object, propertyKey: string, descriptor?: PropertyDescriptor) => void;
 	(debounceTime: number, options?: IDebounceOptions): (target: Object, propertyKey: string, descriptor?: PropertyDescriptor) => void;
 	(target: Object, propertyKey: string, descriptor?: PropertyDescriptor): void;
 }
